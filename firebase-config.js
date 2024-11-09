7aca330fa22ed84fe0c3b2c8d6f30d5dd477057e
Jordan Ye <yejordan8888@gmail.com>
2024-11-09 13:58:37 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPUwZY...",
+  authDomain: "community-f4809.firebaseapp.com",
+  projectId: "community-f4809",
+  storageBucket: "community-f4809.appspot.com",
+  messagingSenderId: "187824225426",
+  appId: "1:187824225426:web:ab220ed4726e4996eafa4b",
+  measurementId: "G-9WS3EKB2NE"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { auth, db };
