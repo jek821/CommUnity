5c34425bfc5d1723e974713b4141c7647ebd3e71
Jordan Ye <yejordan8888@gmail.com>
2024-11-09 21:25:07 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPUwZY...",
-  authDomain: "community-f4809.firebaseapp.com",
-  projectId: "community-f4809",
-  storageBucket: "community-f4809.appspot.com",
-  messagingSenderId: "187824225426",
-  appId: "1:187824225426:web:ab220ed4726e4996eafa4b",
-  measurementId: "G-9WS3EKB2NE"
-};
+    apiKey: "AIzaSyDPUwZYkuKiwuUV-6e4WunUfxvgUL46pgw",
+    authDomain: "community-f4809.firebaseapp.com",
+    projectId: "community-f4809",
+    storageBucket: "community-f4809.firebasestorage.app",
+    messagingSenderId: "187824225426",
+    appId: "1:187824225426:web:ab220ed4726e4996eafa4b",
+    measurementId: "G-9WS3EKB2NE"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
